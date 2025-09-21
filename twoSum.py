563d202817f9442e938463ae9dd2c60680990368
Vishal Jain <jain.vishal@ibm.com>
2025-09-22 00:04:10 +0530

    fixed twosum

--- twoSum.py
+++ twoSum.py
@@ -2,10 +2,9 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         count = {}
         for i, num in enumerate(nums):
-            count[num] = i
             if target - num in count:
-                return [count[target - num], count[num]]
-        return [0,0]
+                return [count[target - num], i]
+            count[num] = i
     
 sol = Solution()
 print(sol.twoSum([4,5,6], 10))
